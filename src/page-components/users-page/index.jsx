99588936a8bd9305450c67b889853c486741302d
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2022-09-16 11:36:28 -0400

    feat: Add mock code for list users

--- src/page-components/users-page/index.jsx
+++ src/page-components/users-page/index.jsx
@@ -1,18 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Navbar from '../../components/navbar';
+import getUsers from '../../services/list-users';
+
+function UserCard({ userData }) {
+  return (
+    <div className="bg-gray-200 px-4 py-2 rounded-lg shadow-lg flex flex-col gap-2">
+      <span className="text-md">{userData.login}</span>
+    </div>
+  );
+}
 
 function UsersPage() {
-  const mockUsers = [
-    {
-      name: 'Samuel',
-      username: 'samueldsr',
-    },
-    {
-      name: 'Risbel',
-      username: 'risbelsr',
-    },
-  ];
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((data) => setUsers(data));
+  }, []);
 
   return (
     <div className="bg-slate-800 min-h-screen">
@@ -24,17 +29,14 @@ function UsersPage() {
         <br />
       </div>
 
-      <div className="flex flex-col gap-4 px-2 mt-4 w-full md:w-1/2">
-        {mockUsers.map((user) => (
-          <div className="bg-gray-200 px-4 py-2 rounded-lg shadow-lg" key={user.username}>
-            <Link to={`/users/${user.username}`}>
-              <span>{user.name}</span>
-              <strong className="ml-2">{user.username}</strong>
+      <div className="flex flex-col gap-4 px-2 mt-4 w-full md:w-1/2 pb-8">
+        {users.length > 0 // Si hay algún user (si vino de la api)
+          && users.map((user) => (
+            <Link to={`/users/${user.login}`}>
+              <UserCard userData={user} />
             </Link>
-          </div>
-        ))}
+          ))}
       </div>
-
     </div>
   );
 }
