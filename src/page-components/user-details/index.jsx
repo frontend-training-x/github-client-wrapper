c203abf26c9ac820b04e837685fb423fdc50a171
Risbel Suarez <risbel961019@gmail.com>
2022-09-27 11:08:21 -0500

    feat: Add UserRepos component in the details

--- src/page-components/user-details/index.jsx
+++ src/page-components/user-details/index.jsx
@@ -3,10 +3,115 @@ import { useParams } from 'react-router-dom';
 import getUsersDetails from '../../services/details-user';
 
 import Navbar from '../../components/navbar/navbar';
+import getUsersRepos from '../../services/repos-user';
+import { MapPinIcon, UsersIcon, StarIcon } from '@heroicons/react/24/outline';
+
+function RepoContainer({ repo }) {
+  return (
+    <div className="border-solid border-2 rounded-md my-4 mx-4 px-2 py-2">
+      <div className="flex flex-wrap">
+        <div>
+          <div className="text-xl text-sky-700 font-semibold mr-4">
+            {repo.name}
+          </div>
+        </div>
+        <div className="text-xs border border-solid border-gray-400 rounded-full bg-white text-center px-2 py-1">
+          {repo.visibility}
+        </div>
+      </div>
+      <div className="text-sm text-gray-700">
+        {repo.description}
+      </div>
+      <div className="flex flex-wrap space-x-4 text-sm text-gray-700">
+        <div className="font-semibold text-gray-800">
+          {repo.language}
+        </div>
+        <div className="flex">
+          <StarIcon className="h-5 w-5 text-black mr-1" />
+          {repo.stargazers_count}
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+function UserRepos({ username }) {
+  const [repos, setRepos] = useState(null);
+
+  useEffect(() => {
+    getUsersRepos(username).then((data) => setRepos(data));
+  }, []);
+
+  return (
+    <div>
+      {
+        repos !== null
+        && repos.map((repo) => (
+          <div key={repo.id}>
+            <RepoContainer repo={repo} />
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+function UserDetails({ users }) {
+  return (
+    <div className="grid grid-cols-3 my-4">
+      <div className="col-start-2 justify-items-center">
+        <img className="rounded-full border-solid border-gray-200 border-x-8" src={users.avatar_url} alt={users.name} />
+      </div>
+      <div className="col-start-2 text-center text-xl">{users.name}</div>
+      <div className="col-start-2 text-center">{users.login}</div>
+      <div className="col-start-1 mt-4 mx-4">
+        <div className="mx-2 my-4">
+          {users.bio}
+        </div>
+        <div className="flex flex-wrap">
+          <UsersIcon className="h-6 w-6 mr-1" />
+          Fallowers:
+          {' '}
+          <div className="font-semibold mx-1">
+            {users.followers}
+          </div>
+          /
+          Following:
+          <div className="font-semibold mx-1">
+            {users.following}
+          </div>
+        </div>
+        <div className="flex">
+          <MapPinIcon className="h-6 w-6 mr-1" />
+          {users.location}
+        </div>
+      </div>
+      <div className="col-start-2 col-span-2 mt-4">
+        <div className="ml-4">
+          Repos:
+          <div className="flex">
+            Public repos:
+            <dir className="font-semibold px-1 my-0">
+              {users.public_repos}
+            </dir>
+          </div>
+          <div className="flex">
+            Public gists:
+            <dir className="font-semibold px-1 my-0">
+              {users.public_gists}
+            </dir>
+          </div>
+        </div>
+        <UserRepos username={users.login} />
+      </div>
+    </div>
+  );
+}
 
 function UserPage() {
   const { username } = useParams();
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
     getUsersDetails(username).then((data) => setUsers(data));
@@ -15,21 +120,10 @@ function UserPage() {
   return (
     <div className="bg-violet-300">
       <Navbar />
-      <div className="grid grid-cols-3 my-4 justify-items-center">
-        <img className="col-start-2 rounded-full border-solid border-gray-200 border-x-8" src={users.avatar_url} alt={users.name} />
-        <div className="col-start-2 text-center text-xl">{users.name}</div>
-        <div className="col-start-2 text-center">{users.login}</div>
-        <div className="col-start-1 text-center mt-4">
-          Public repos:
-          {' '}
-          {users.public_repos}
-        </div>
-        <div className="col-start-1 text-center">
-          Public gists:
-          {' '}
-          {users.public_gists}
-        </div>
-      </div>
+      {
+      users !== null
+      && <UserDetails users={users} />
+      }
     </div>
   );
 }
--- src/page-components/users-list/index.jsx
+++ src/page-components/users-list/index.jsx
@@ -9,7 +9,7 @@ function UserCard({ userData }) {
   return (
     <div className="bg-gray-300 rounded-lg shadow-lg flex">
       <Link className="flex" to={`/users/${userData.login}`}>
-        <img className="rounded-l-lg w-20 h-22" alt={userData.avatar_url} src={userData.avatar_url} />
+        <img className="rounded-l-lg w-20 h-22" alt={userData.avatar_url} src={userData.avatar} />
         <div className="bg-gray-100  flex flex-col justify-center  px-2 py-2 gap-3">
           <div className="text-md font-medium">
             <div>
@@ -25,23 +25,6 @@ function UserCard({ userData }) {
           </div>
         </div>
       </Link>
-      <div className="text-md font-medium mx-2 px-2 py-2  bg-blue-300 shadow-lg flex flex-col">
-        <div>
-          Location:
-          {' '}
-          {userData.location}
-        </div>
-        <div>
-          Followers:
-          {' '}
-          {userData.followers}
-        </div>
-        <div>
-          Fallowing:
-          {' '}
-          {userData.following}
-        </div>
-      </div>
     </div>
   );
 }
@@ -52,15 +35,13 @@ function UsersPage() {
   const [users, setUsers] = useState([]);
 
   const handleSetUsersDetailsData = (userData) => Promise.all(
-    userData.map(async (userData) => {
-      const userDetails = await getUserDetails(userData.login);
+    userData.map(async (user) => {
+      const userDetails = await getUserDetails(user.login);
 
       return {
-        ...userData,
-        followers: userDetails.followers || 0,
-        following: userDetails.following || 0,
-        location: userDetails.location || 'Reserved',
+        ...user,
         name: userDetails.name || ' ',
+        avatar: userDetails.avatar_url,
       };
     }),
   );
@@ -75,7 +56,7 @@ function UsersPage() {
       <div className="flex flex-wrap">
         {users.length > 0 && users.map((user) => (// Si hay algún user (si vino de la api)
           <div key={user.id} className="flex mx-2 my-3">
-            <UserCard userData={user} />
+            <UserCard key={user.id} userData={user} />
           </div>
         ))}
       </div>
