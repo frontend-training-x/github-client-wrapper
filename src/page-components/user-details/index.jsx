6786f8873d7a7bd007b78f33ae9d6538a5d6ba77
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2022-09-30 11:57:01 -0400

    lint: apply lint to file

--- src/page-components/user-details/index.jsx
+++ src/page-components/user-details/index.jsx
@@ -1,37 +1,30 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { MapPinIcon, UsersIcon, StarIcon } from '@heroicons/react/24/outline';
 import getUserDetails from '../../services/details-user';
 
 import Navbar from '../../components/navbar/navbar';
 import getUsersRepos from '../../services/repos-user';
-import { MapPinIcon, UsersIcon, StarIcon } from '@heroicons/react/24/outline';
 
 function RepoContainer({ repo }) {
   return (
     <div className="border-solid border-2 rounded-md my-4 mx-4 px-2 py-2">
       <div className="flex flex-wrap">
         <div>
-          <div className="text-xl text-sky-700 font-semibold mr-4">
-            {repo.name}
-          </div>
+          <div className="text-xl text-sky-700 font-semibold mr-4">{repo.name}</div>
         </div>
         <div className="text-xs border border-solid border-gray-400 rounded-full bg-white text-center px-2 py-1">
           {repo.visibility}
         </div>
       </div>
-      <div className="text-sm text-gray-700">
-        {repo.description}
-      </div>
+      <div className="text-sm text-gray-700">{repo.description}</div>
       <div className="flex flex-wrap space-x-4 text-sm text-gray-700">
-        <div className="font-semibold text-gray-800">
-          {repo.language}
-        </div>
+        <div className="font-semibold text-gray-800">{repo.language}</div>
         <div className="flex">
           <StarIcon className="h-5 w-5 text-black mr-1" />
           {repo.stargazers_count}
         </div>
       </div>
-
     </div>
   );
 }
@@ -45,14 +38,12 @@ function UserRepos({ username }) {
 
   return (
     <div>
-      {
-        repos !== null
+      {repos !== null
         && repos.map((repo) => (
           <div key={repo.id}>
             <RepoContainer repo={repo} />
           </div>
-        ))
-      }
+        ))}
     </div>
   );
 }
@@ -66,21 +57,14 @@ function UserDetails({ users }) {
       <div className="col-start-2 text-center text-xl">{users.name}</div>
       <div className="col-start-2 text-center">{users.login}</div>
       <div className="col-start-1 mt-4 mx-4">
-        <div className="mx-2 my-4">
-          {users.bio}
-        </div>
+        <div className="mx-2 my-4">{users.bio}</div>
         <div className="flex flex-wrap">
           <UsersIcon className="h-6 w-6 mr-1" />
           Fallowers:
           {' '}
-          <div className="font-semibold mx-1">
-            {users.followers}
-          </div>
-          /
-          Following:
-          <div className="font-semibold mx-1">
-            {users.following}
-          </div>
+          <div className="font-semibold mx-1">{users.followers}</div>
+          / Following:
+          <div className="font-semibold mx-1">{users.following}</div>
         </div>
         <div className="flex">
           <MapPinIcon className="h-6 w-6 mr-1" />
@@ -92,15 +76,11 @@ function UserDetails({ users }) {
           Repos:
           <div className="flex">
             Public repos:
-            <dir className="font-semibold px-1 my-0">
-              {users.public_repos}
-            </dir>
+            <dir className="font-semibold px-1 my-0">{users.public_repos}</dir>
           </div>
           <div className="flex">
             Public gists:
-            <dir className="font-semibold px-1 my-0">
-              {users.public_gists}
-            </dir>
+            <dir className="font-semibold px-1 my-0">{users.public_gists}</dir>
           </div>
         </div>
         <UserRepos username={users.login} />
@@ -120,10 +100,7 @@ function UserPage() {
   return (
     <div className="bg-violet-300">
       <Navbar />
-      {
-      users !== null
-      && <UserDetails users={users} />
-      }
+      {users !== null && <UserDetails users={users} />}
     </div>
   );
 }
